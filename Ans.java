2a967e5c2856302166ff2043604de4fe215be64a
tejaswinagidi <62585607+tejaswinagidi@users.noreply.github.com>
2020-03-31 18:22:40 +0530

    Add files via upload

---
+++ Ans.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Ans {
+
+    public static void main(String[] args) {
+        double myDouble = 1.1;
+        int myInt = (int) myDouble;
+         
+       //assertNotEquals(myDouble, myInt);
+        System.out.println(myDouble+" "+myInt);
+    }
+}
+
