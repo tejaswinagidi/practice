c51da2c4c1d36caff4ba978ab36b4808675ceeb6
tejaswinagidi <62585607+tejaswinagidi@users.noreply.github.com>
2020-04-03 11:56:35 +0530

    Add files via upload

---
+++ StringBufferEx.java
@@ -0,0 +1,7 @@
+    class StringBufferEx{  
+    public static void main(String args[]){  
+    String sb=new String("Hello ");  
+    sb.append("Java");//now original string is changed  
+    System.out.println(sb);//prints Hello Java  
+    }  
+    }  
